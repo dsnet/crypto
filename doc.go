09a8f8019a53e04120309b77c44797acc72bb4ed
Joe Tsai <joetsai@digital-static.net>
2014-08-27 18:19:05 -0700

    Add top level package file

---
+++ doc.go
@@ -0,0 +1,6 @@
+// Copyright 2014, Joe Tsai. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE.md file.
+
+// Package gorandom implements a parallelized PRNG.
+package gorandom
