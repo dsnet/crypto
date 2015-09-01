811f5b7c5adf14b1c60e49391f2cd1bd2af3f456
Joe Tsai <joetsai@digital-static.net>
2015-09-01 01:43:10 -0700

    Fix spelling mistake

--- doc.go
+++ doc.go
@@ -2,5 +2,5 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE.md file.
 
-// Pacakge crypto is a collection of crypto libraries.
+// Package crypto is a collection of crypto libraries.
 package crypto
