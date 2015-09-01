752aeedfe7aac0ada0102ea6f3ee53f55c584d54
Joe Tsai <joetsai@digital-static.net>
2015-09-01 01:59:39 -0700

    Update doc.go

--- doc.go
+++ doc.go
@@ -2,5 +2,5 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE.md file.
 
-// Package crypto is a collection of crypto libraries.
+// Package crypto is a collection of cryptography libraries.
 package crypto
